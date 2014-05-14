0e9648fc5a65874552727864cba2fa148af676aa
wilson <wilson20120311@gmail.com>
2014-05-14 21:54:03 +0800

    first commit

---
+++ view/handler.py
@@ -0,0 +1,24 @@
+import jinja2
+import webapp2
+import dot
+
+import codecs
+import os
+
+
+def js_template(js_file):
+    return dot.template(codecs.open(js_file,'r','utf8').read())
+
+
+class HtmlHandler(webapp2.RequestHandler):
+    jinja_environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('web')
+    )
+    jinja_environment.globals['js_template'] = js_template
+
+
+
+    def HtmlResponse(self, template_path, data):
+        template = self.jinja_environment.get_template(template_path)
+        self.response.out.write(template.render(data))
+
