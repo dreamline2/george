ce3f92af77ba215e5faa85998aab35b9c5f5f58f
wilson <wilson20120311@gmail.com>
2014-05-27 18:43:20 +0800

    首頁第一版

--- george/static/js/config.js
+++ george/static/js/config.js
@@ -5,21 +5,15 @@ window._skel_config = {
 	boxModel: 'border',
 	grid: { gutters: 30 },
 	breakpoints: {
-		wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
-		narrow: { range: '481-1199', containers: 960 },
-		narrower: { range: '481-960', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 20 }},
-		mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 20 } }
+		wide: { range: '1200-', containers: '90%', grid: { gutters: 0 } },
+		narrow: { range: '961-1199', containers: 960 },
+		narrower: { range: '481-960', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 0 }},
+		mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 0 } }
 	}
 };
 
 window._skel_panels_config = {
 	panels: {
-		leftPanel: {
-			breakpoints: 'narrower,mobile',
-			position: 'left',
-			size: 250,
-			html: '<div data-action="moveCell" data-args="left-sidebar,content"></div>'
-		},
 		rightPanel: {
 			breakpoints: 'narrower,mobile',
 			position: 'right',
@@ -37,33 +31,26 @@ window._skel_panels_config = {
 	},
 	overlays: {
 		titleBar: {
-			breakpoints: 'mobile',
+			breakpoints: 'wide,narrow,narrower,mobile',
 			position: 'top-center',
 			width: '100%',
 			height: 44,
 			html: '<div data-action="copyHTML" data-args="title"></div>'
 		},
-		contactBar: {
-			position: 'bottom-center',
-			width: '100%',
-			height: 60,
-			html:	'<a href="http://twitter.com/n33co" class="icon-twitter"></a>' +
-					'<a href="#" class="toggle icon-envelope" data-action="togglePanel" data-args="bottomPanel"></a>' +
-					'<a href="http://twitter.com/n33co" class="icon-facebook"></a>'
-		},
-		leftPanelButton: {
-			breakpoints: 'narrower,mobile',
-			position: 'top-left',
-			width: 80,
-			height: 60,
-			html: '<div class="toggle icon-angle-right" data-action="togglePanel" data-args="leftPanel"></div>'
-		},
+		// contactBar: {
+		// 	position: 'bottom-center',
+		// 	width: '100%',
+		// 	height: 60,
+		// 	html:	'<a href="http://twitter.com/n33co" class="icon-twitter"></a>' +
+		// 			'<a href="#" class="toggle icon-envelope" data-action="togglePanel" data-args="bottomPanel"></a>' +
+		// 			'<a href="http://twitter.com/n33co" class="icon-facebook"></a>'
+		// },
 		rightPanelButton: {
 			breakpoints: 'narrower,mobile',
 			position: 'top-right',
 			width: 80,
 			height: 60,
-			html: '<div class="toggle icon-angle-left" data-action="togglePanel" data-args="rightPanel"></div>'
+			html: '<div class="toggle icon-reorder" data-action="togglePanel" data-args="rightPanel"></div>'
 		}
 	}
 };
